02f7d883999a5917d686318da7b5cd7648d2d163
Cit-Yo <vitor_cit@hotmail.com>
2024-09-06 15:09:41 -0300

    Versão mais complexa de main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import random
+
+num = random.randint(1,3)
+
+print (num)
+if num == 1:
+    rand = "Person!"
+elif num == 2:
+    rand = "Mister!"
+else:
+    rand = "Madam!"
+
+print("Hello, "+ rand)
