c17ee9ad2dcd0e74af30bbddd9d60b66cf782580
Cit-Yo <vitor_cit@hotmail.com>
2024-09-26 17:07:32 -0300

    CI testes

---
+++ main.py
@@ -0,0 +1,10 @@
+def genderSelect(genderValue):
+
+    if genderValue == "M":
+        gender = "Male"
+    elif genderValue == "F":
+        gender = "Female"
+    else:
+        print("Invalid.")
+
+    return gender
---
+++ test.py
@@ -0,0 +1,11 @@
+from main import genderSelect
+from unittest.mock import patch
+
+def test_genderSelect():
+    
+    gender_test = "Male"
+    assert gender_test == genderSelect("M")
+    gender_test = "Female"
+    assert gender_test == genderSelect("F")
+
+test_genderSelect()
